75a43468bd006a6c8358dfddfd9170e419e25faf
Jay sharma <jaysharma895@gmail.com>
2024-12-22 22:12:49 +0530

    initialeze my portfolio website react code

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+
+function App() {
+  return (
+    <>
+    <section classname="">
+      <div className="h-screen bg-slate-950 text-white">
+        <div className="header h-10 bg-slate-900 flex justify-between">
+          <img src="/js.jpg" alt="logo-of-jay" className="w-10"/>
+          <h2 className="p-3">I am heaader</h2>
+        </div>
+      </div>
+    </section>
+    <section classname="">
+      <div className="h-screen bg-slate-950 text-white">About me</div>
+    </section>
+    <section classname="">
+      <div className="h-screen bg-slate-950 text-white">Services</div>
+    </section>
+    <section classname="">
+      <div className="h-screen bg-slate-950 text-white">Projects</div>
+    </section>
+    </>
+  );
+}
+
+export default App;
